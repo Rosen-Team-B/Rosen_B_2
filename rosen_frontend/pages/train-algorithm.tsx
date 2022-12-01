49ac8597aabf58213a182649fd41ce0abc696732
yuqi88 <snow34545@gmail.com>
2022-11-30 21:55:00 -0800

    add page: train-algorithm

---
+++ rosen_frontend/pages/train-algorithm.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TrainAlgorithm = () => {
+    return (
+        <div>
+            This should be the stepper page
+        </div>
+    );
+};
+
+export default TrainAlgorithm;
