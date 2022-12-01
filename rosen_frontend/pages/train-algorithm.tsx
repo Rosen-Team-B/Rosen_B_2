5284676eb9ce98bcd96d9cb482893f4a415a0ba5
yuqi88 <snow34545@gmail.com>
2022-12-01 00:08:21 -0800

    add component progressive stepper

--- rosen_frontend/pages/train-algorithm.tsx
+++ rosen_frontend/pages/train-algorithm.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import ProgressiveStepper from "../components/ProgressiveStepper";
 
 const TrainAlgorithm = () => {
+
+    
     return (
         <div>
             This should be the stepper page
+            <ProgressiveStepper></ProgressiveStepper>
         </div>
     );
 };
