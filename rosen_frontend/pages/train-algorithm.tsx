b8d49cc81dd65a2427bda14cca8a95f850bbe512
yuqi88 <snow34545@gmail.com>
2022-12-01 00:32:46 -0800

    move stepper into the train-algo page

--- rosen_frontend/pages/train-algorithm.tsx
+++ rosen_frontend/pages/train-algorithm.tsx
@@ -1,13 +1,46 @@
 import React from 'react';
 import ProgressiveStepper from "../components/ProgressiveStepper";
+import {Button, Step, StepLabel, Stepper} from "@mui/material";
+
+const stepperSteps = [
+    "Step 1: Video in Database",
+    "Step 2: Video Parsing",
+    "Step 3: Image Vectorizing",
+    "Step 4: Upload the Reference",
+];
 
 const TrainAlgorithm = () => {
+    const [activeStep, setActiveStep] = React.useState(0);
 
-    
+    /** Handle Functions */
+    const nextButton = () => {
+        activeStep < stepperSteps.length - 1
+            ? setActiveStep(activeStep + 1)
+            : setActiveStep(activeStep);
+    }
+
+    const backButton = () => {
+        activeStep >= 0 ? setActiveStep(activeStep - 1) : setActiveStep(activeStep);
+    }
+
+
     return (
         <div>
             This should be the stepper page
-            <ProgressiveStepper></ProgressiveStepper>
+            <Stepper activeStep={activeStep} alternativeLabel>
+                {stepperSteps.map((label) => (
+                    <Step key={label}>
+                        <StepLabel>{label}</StepLabel>
+                    </Step>
+                ))}
+            </Stepper>
+
+            <Button onClick={backButton}>
+                Back
+            </Button>
+            <Button onClick={nextButton}>
+                {activeStep === stepperSteps.length - 1 ? 'Finish' : 'Next'}
+            </Button>
         </div>
     );
 };
