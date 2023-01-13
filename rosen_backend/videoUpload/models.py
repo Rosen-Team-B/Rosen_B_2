0717903228a64b05c354ac1822ab824835bc267e
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-01-13 15:55:35 -0800

    Adding model and updating admin page

--- rosen_backend/videoUpload/models.py
+++ rosen_backend/videoUpload/models.py
@@ -3,3 +3,6 @@ from django.db import models
 # Create your models here.
 class VideoUploadModel(models.Model):
     video = models.FileField(upload_to="refVideoUpload")
+
+class ImageUploadModel(models.Model):
+    image = models.ImageField(upload_to="refImageUpload")
