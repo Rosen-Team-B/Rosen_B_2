6174e9d768a14dcf805d2a0f4da75fed379d6531
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-01-13 15:57:14 -0800

    Adding image serializer

--- rosen_backend/videoUpload/serializers.py
+++ rosen_backend/videoUpload/serializers.py
@@ -1,8 +1,13 @@
 from django.core import serializers
-from .models import VideoUploadModel
+from .models import VideoUploadModel,ImageUploadModel
 from rest_framework import serializers
 
 class VideoUploadModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = VideoUploadModel
         fields = ('video')
+
+class ImageUploadModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ImageUploadModel
+        fields = ('image')
