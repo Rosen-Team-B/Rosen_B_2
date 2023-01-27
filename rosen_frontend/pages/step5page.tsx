185855eb78e279363828571bbd0d8daf3a77a649
Jeena01 <jeenajavahar@gmail.com>
2023-01-26 17:42:36 -0800

    working on it

--- rosen_frontend/pages/step5page.tsx
+++ rosen_frontend/pages/step5page.tsx
@@ -1,10 +1,44 @@
+import { Button } from "@mui/material";
+import SelectableImage from "./selectableImage";
+
 const step5page = () =>{
 
+    var ImageData: string[] = [];
+
+    const fetchImages = (k:number) =>{
+        ImageData=[];
+        for (var i = 1; i <= k; i++) {
+            ImageData.push("name"+i);
+         }
+    }
+    const getImageGallery= () =>{
+        return(
+            <div>
+                {
+                    ImageData.map((img)=>(
+                        SelectableImage(img,img)
+                    )
+                    )
+                }
+            </div>
+        )
+    }
 
     return(
         <div>
+            <Button>
+                Fetch Images
+            </Button>
+
         </div>
     )
 }
 
 export default step5page;
+
+
+/*
+
+
+
+*/
