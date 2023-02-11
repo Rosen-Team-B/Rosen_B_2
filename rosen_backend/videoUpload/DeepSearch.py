62d3220551037b2a404b6f53665bd78c57bf4b16
Brenden-Trieu <59781947+Brenden-Trieu@users.noreply.github.com>
2023-02-10 17:58:28 -0800

    ipynb transfer testing

--- rosen_backend/videoUpload/DeepSearch.py
+++ rosen_backend/videoUpload/DeepSearch.py
@@ -2,7 +2,10 @@ from DeepImageSearch import Index, LoadData, SearchImage
 
 
 def start():
-    print('hi')
+    image_list = LoadData().from_folder(['../media/video_frames/'])
+    Index(image_list).Start()
+    SearchImage().get_similar_images(image_list[9],6)
+    SearchImage().plot_similar_images(image_list[9])
 
 if __name__ == "__main__":
     start()
