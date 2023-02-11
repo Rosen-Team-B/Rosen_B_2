641ff2256d1d174cdaefd1c462b06d82433240e9
Brenden-Trieu <59781947+Brenden-Trieu@users.noreply.github.com>
2023-02-10 18:02:32 -0800

    Update DeepSearch.py

--- rosen_backend/videoUpload/DeepSearch.py
+++ rosen_backend/videoUpload/DeepSearch.py
@@ -5,7 +5,8 @@ def start():
     image_list = LoadData().from_folder(['../media/video_frames/'])
     Index(image_list).Start()
     SearchImage().get_similar_images(image_list[9],6)
-    SearchImage().plot_similar_images(image_list[9])
+    #SearchImage().plot_similar_images(image_list[9])
+    print("image list complete")
 
 if __name__ == "__main__":
     start()
