b8c5071f07da1f68492927bc4d4eeb7407cffb8c
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-02-10 17:41:43 -0800

    Adding temp changes

--- rosen_backend/videoUpload/signals.py
+++ rosen_backend/videoUpload/signals.py
@@ -1,9 +1,11 @@
 from django.db.models.signals import post_save
 from django.core.files.base import ContentFile
+from django.core.files.images import ImageFile
 from django.dispatch import receiver
 from . models import VideoModel
 from . serializers import ImageFrameModelSerializer
 import cv2
+import io
 from PIL import Image as im
 import pdb
 
@@ -19,10 +21,11 @@ def video_parser(sender, **kwargs):
         if (count%1000==0):
             #cv2.imwrite("./media/video_frames/frame%d.jpg" % count, image)
             data = im.fromarray(image)
+            imagetest = ImageFile(io.BytesIO(data.tobytes()), "frame%d.png" % count)
             #This line below works
-            data.save("C:/Users/alila/Desktop/COSC 499/Rosen_B_2/rosen_backend/media/video_frames/frame%dtest.png" % count)
+            #data.save("C:/Users/alila/Desktop/COSC 499/Rosen_B_2/rosen_backend/media/video_frames/frame%dtest.png" % count)
             #This line still needs work to be done to it
-            serializer = ImageFrameModelSerializer(data={"image":ContentFile(data.tobytes(), "frame%d.png" % count)})
+            serializer = ImageFrameModelSerializer(data={"image":imagetest})
             serializer.is_valid()
             serializer.save()
         count += 1
