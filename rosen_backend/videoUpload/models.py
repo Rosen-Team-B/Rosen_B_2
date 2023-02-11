3a17717f134d43729a9849c60b85390c70423a6a
Ali Ladha <59992569+thenewguy321@users.noreply.github.com>
2023-02-10 17:57:32 -0800

    Revert incorrect model change

--- rosen_backend/videoUpload/models.py
+++ rosen_backend/videoUpload/models.py
@@ -5,9 +5,9 @@ class VideoModel(models.Model):
     video = models.FileField(upload_to="refVideoUpload")
 
 class ReferenceImageModel(models.Model):
-    image = models.ImageField(upload_to="refImageUpload")
+    image = models.FileField(upload_to="refImageUpload")
     label = models.CharField(max_length=100,blank=True, default='')
 
 class ImageFrameModel(models.Model):
-    image = models.ImageField(upload_to="video_frames")
+    image = models.FileField(upload_to="video_frames")
     timestamp = models.CharField(max_length=100,blank=True, default='')
