8129c864b85475c68528612ed32dd2ead31d4167
Jeena01 <jeenajavahar@gmail.com>
2023-02-13 14:40:14 -0800

    Update 4.tsx

--- rosen_frontend/pages/train-algorithm/4.tsx
+++ rosen_frontend/pages/train-algorithm/4.tsx
@@ -8,7 +8,8 @@ const Step4 =()=>{
     const description= stepperTexts[activeStep];
     const router=useRouter();
 
-    let video:File;
+    let image:File;
+    let label="label1";
     const currentPage=4;
     const nextButton = () => {
         //const fs = require('fs');
@@ -19,8 +20,9 @@ const Step4 =()=>{
     const onSubmit= async(e: React.FormEvent<HTMLFormElement>)=>{
         e.preventDefault();
         const formData=new FormData();
-        formData.append('video', video, 'toronto.mp4');
-        fetch('http://127.0.0.1:8000/videoUpload/video/', {
+        formData.append('image', image);
+        formData.append('label',label);
+        fetch('http://127.0.0.1:8000/videoUpload/image/', {
             method:"POST",
             body: formData,
             mode: "no-cors",
@@ -31,16 +33,12 @@ const Step4 =()=>{
     }
     const onChange=(e: React.ChangeEvent<HTMLInputElement>)=>{
         if(e.target.files!=null){
-           video=e.target.files[0];
-           
+           image=e.target.files[0];
         }
         
     }
     const getPage=()=>{
-        // const fs = require('fs');
-        // const fileContents = fs.readFileSync("../../utils/pagenumber.txt", { encoding: "utf8" })
-        // return parseInt(fileContents);
-        return 1;
+        return 4;
     }
     useEffect(() => {
         const page=getPage();
@@ -50,6 +48,10 @@ const Step4 =()=>{
             router.push('/train-algorithm/'+page);
         }
       }, []);
+
+    const labelUpdate=(e: React.ChangeEvent<HTMLInputElement>)=> {
+        label=e.target.value;
+    }
 
     return (
         <div>
@@ -66,6 +68,7 @@ const Step4 =()=>{
                         <br />
                         <form id="ref-image-upload" encType="multipart/form-data" onSubmit={(e) =>onSubmit(e)}>
                             <input type="file" name="image" accept="image/*"required onChange={(e)=>onChange(e)}/>
+                            <input type="text" name="label" value={label} onChange={(e)=>labelUpdate(e)}  />
                             <Button type="submit">
                                 Submit
                             </Button>
