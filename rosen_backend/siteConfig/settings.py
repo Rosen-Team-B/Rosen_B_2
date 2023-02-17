979d7afab8211b6b64512e31567f254b57ad469c
yuqi88 <snow34545@gmail.com>
2023-02-17 10:06:43 -0800

    remove django and default db connection

--- rosen_backend/siteConfig/settings.py
+++ rosen_backend/siteConfig/settings.py
@@ -76,13 +76,6 @@ WSGI_APPLICATION = 'siteConfig.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
