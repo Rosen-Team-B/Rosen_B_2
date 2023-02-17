1459cdd0e941efff8d8171edb53b6baa6bdacbbd
yuqi88 <snow34545@gmail.com>
2023-02-17 10:10:50 -0800

    put default back for Ali

--- rosen_backend/siteConfig/settings.py
+++ rosen_backend/siteConfig/settings.py
@@ -76,6 +76,13 @@ WSGI_APPLICATION = 'siteConfig.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
