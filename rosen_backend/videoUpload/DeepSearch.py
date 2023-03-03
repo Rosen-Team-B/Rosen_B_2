85d1183c564b62d50832c52c4ed59d8ad1a7d2b3
Brenden-Trieu <59781947+Brenden-Trieu@users.noreply.github.com>
2023-03-02 21:21:31 -0800

    Update DeepSearch.py

--- rosen_backend/videoUpload/DeepSearch.py
+++ rosen_backend/videoUpload/DeepSearch.py
@@ -2,6 +2,6 @@ from DeepImageSearch import Index, LoadData, SearchImage
 import os
 
 def DeepSearch():
-    image_list = LoadData().from_folder(['rosen_backend/media/refImageUpload/'])
+    image_list = LoadData().from_folder(['rosen_backend/media/video_frames/'])
     Index(image_list).Start()
     print(SearchImage().get_similar_images(image_list[0],6))
