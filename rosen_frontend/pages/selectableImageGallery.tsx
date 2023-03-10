765ca28b21c82aeee49413051d3caaf2c82806dc
Jeena01 <jeenajavahar@gmail.com>
2023-03-10 10:21:28 -0800

    Update selectableImageGallery.tsx

--- rosen_frontend/pages/selectableImageGallery.tsx
+++ rosen_frontend/pages/selectableImageGallery.tsx
@@ -6,21 +6,9 @@ import SelectableImage from "../components/ProgressiveStepper/selectableImage";
 const SelectableImageGallery = () => {
   const paths=["myimage.jpg","myimage.jpg","myimage.jpg","myimage.jpg"];;
   return (
-    <Box
-      display="flex"
-      gap={2}
-      sx={{
-        float: "left",
-        width: "25%",
-      }}
-    >
       <Grid container rowSpacing={1} columnSpacing={1} >
       {paths.map((path) => <Grid item> {SelectableImage(path)} </Grid>)}
       </Grid>
-      
-    </Box>
-
-
 
   );
 };
