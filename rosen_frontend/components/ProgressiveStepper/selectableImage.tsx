d2e48f6708363485a5d069c76c65e0169843859e
Jeena01 <jeenajavahar@gmail.com>
2023-03-10 10:33:35 -0800

    Update selectableImage.tsx

--- rosen_frontend/components/ProgressiveStepper/selectableImage.tsx
+++ rosen_frontend/components/ProgressiveStepper/selectableImage.tsx
@@ -1,4 +1,4 @@
-import  React, { useEffect } from 'react';
+import  React, { useEffect, useRef } from 'react';
 import { Box, Card, Checkbox } from "@mui/material";
 import Button from '@mui/material/Button';
 import Image from 'next/image';
@@ -6,14 +6,19 @@ const SelectableImage= (pathname: string) => {
 
 const width=250;
 const height=250;
+
+
+
+const checkboxref=useRef(null);
+
 const buttonClicked = () => {
     
     console.log("clicked");
 }
     return(
         <Box style={{position : 'relative' , top: '0%',left:'0%'}} sx={{ bgcolor: 'white' ,display: 'flex' , maxWidth: width }}>
-            <img src={pathname} width={width} height={height} alt={'bro'}/>
-           <Checkbox onChange = {buttonClicked} sx= {{position: 'absolute', top: '0%', right:'0%'}}/>
+            <img onClick={buttonClicked} src={pathname} width={width} height={height} alt={'bro'}/>
+           <Checkbox ref={checkboxref} sx= {{position: 'absolute', top: '0%', right:'0%'}}/>
            
         </Box>
         
