1e344544ee8d333712314403a2defe881609f591
Jeena01 <jeenajavahar@gmail.com>
2023-03-14 21:47:26 -0700

    Update index.tsx

--- rosen_frontend/pages/index.tsx
+++ rosen_frontend/pages/index.tsx
@@ -10,11 +10,34 @@ export default function Home() {
   const start = () => {
     router.push("/train-algorithm/1");
   };
+  const styles={
+    mainbox:{
+      width: "90%",
+      padding: "5%",
+      margin: "2rem auto 1rem auto",
+    }
+  }
 
   return (
     <div>
       <PageHeader/>
-      <Button onClick={start} >Start Training</Button>
+      <div style={styles.mainbox}>
+        <h1>
+          Get Started
+        </h1>
+        <p>
+          This image-analysis tool is designed for the inspection of water pipelines. 
+        </p>
+        <p>
+          Using a pre-trained learning algorithm, you can detect frames in video footage that are similar to a specific reference image.
+        </p>
+        <p>
+          Click the button below to start inspections!
+        </p>
+        <Button onClick={start} >Start</Button>
+      </div>
+      
+      
     </div>
   )
 }
