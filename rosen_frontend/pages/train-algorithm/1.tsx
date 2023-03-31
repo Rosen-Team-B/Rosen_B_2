a9170f1cd2ba49633f1a88854c71412139c368f8
Jeena01 <jeenajavahar@gmail.com>
2023-03-31 11:30:44 -0700

    Update 1.tsx

--- rosen_frontend/pages/train-algorithm/1.tsx
+++ rosen_frontend/pages/train-algorithm/1.tsx
@@ -12,6 +12,7 @@ const Step1 = () => {
     const description = stepperTexts[activeStep];
     const router = useRouter();
     let video: File;
+    let interval:number;
 
     const currentPage = 1;
     const nextButton = () => {
@@ -25,6 +26,7 @@ const Step1 = () => {
         e.preventDefault();
         const formData = new FormData();
         formData.append('video', video, 'toronto.mp4');
+        //formData.append('interval',interval);
         fetch('http://127.0.0.1:8000/videoUpload/video/', {
                 method: "POST",
                 body: formData,
@@ -32,16 +34,18 @@ const Step1 = () => {
         ).then((res) => setDisableNext(false)).catch((err) => (console.log("error")));
     }
 
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const onVidChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.files != null) {
             video = e.target.files[0];
         }
     }
+    const onIntChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.value != null) {
+            interval = e.target.valueAsNumber;
+        }
+    }
 
     const getPage = () => {
-        // const fs = require('fs');
-        // const fileContents = fs.readFileSync("../../utils/pagenumber.txt", { encoding: "utf8" })
-        // return parseInt(fileContents);
         return 1;
     }
 
@@ -70,7 +74,8 @@ const Step1 = () => {
                 <h3>{description}</h3>
                 <br/>
                 <form id="video-upload" encType="multipart/form-data" onSubmit={(e) => onSubmit(e)}>
-                    <input type="file" name="video" accept="video/*" required onChange={(e) => onChange(e)}/>
+                    <input type="file" name="video" accept="video/*" required onChange={(e) => onVidChange(e)}/>
+                    <input type="number" name= "interval" onChange={(e) => onIntChange(e)}/>
                     <Button type="submit">
                         Submit
                     </Button>
