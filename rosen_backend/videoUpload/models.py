64b1f2d6308c2db4ba01c94c29150c1b9e3467e4
thenewguy321 <aliladha2000@gmail.com>
2023-04-01 19:33:16 -0700

    temp changes

--- rosen_backend/videoUpload/models.py
+++ rosen_backend/videoUpload/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class VideoModel(models.Model):
     video = models.FileField(upload_to="refVideoUpload")
+    name = models.CharField(max_length=100, blank=False, default="")  # todo: make sure this is set
 
 
 class ReferenceImageModel(models.Model):
@@ -13,4 +14,24 @@ class ReferenceImageModel(models.Model):
 
 class ImageFrameModel(models.Model):
     image = models.FileField(upload_to="video_frames")
+    # todo: add a filename field
     timestamp = models.CharField(max_length=100, blank=True, default="")
+    video = models.ForeignKey(VideoModel)  # todo: make sure this is set
+
+    @property
+    def formatted_timestamp(self):
+        """
+        Format the timestamp to not have special characters
+        """
+        return self.timestamp.replace(":","").replace(".","")
+
+    @property
+    def generated_filename(self):
+        return f"{self.video.name}-{self.formatted_timestamp}"
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        """
+        self.filename = self.generated_filename
+        self.save()
+        """
