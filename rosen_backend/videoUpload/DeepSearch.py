49f40f07f95b15df4159aad5fce5163cccb094ee
thenewguy321 <aliladha2000@gmail.com>
2023-04-01 19:33:04 -0700

    Adding temp

--- rosen_backend/videoUpload/DeepSearch.py
+++ rosen_backend/videoUpload/DeepSearch.py
@@ -1,17 +1,21 @@
 from DeepImageSearch import Index, LoadData, SearchImage
 from .models import ImageFrameModel
 import os
-
+#from pdb import set_trace as bp
 
 def create_viewable_links(image_list):
     backend_url = "http://localhost:8000/"
+    print(image_list)
     for key in image_list:
         location = image_list[key].split("/rosen_backend/")[-1]
+        #print(location)
         time = 0.0
-        # image_frame_model = ImageFrameModel.objects.filter(__name__=image_list[key])
-        # if image_frame_model is not None:
-        #     image_list[key].append(image_frame_model.get)
-        json_sub_object = {"name": backend_url + location,"timestamp": time}
+        image_frame_model = ImageFrameModel.objects.all().annotate(
+
+        ).filter(image__exact=str(backend_url + location)).values()
+        if image_frame_model.exists():
+            time = image_frame_model.get("timestamp")
+        json_sub_object = {"location": backend_url + location,"timestamp": time}
         image_list[key] = json_sub_object
     return image_list
 
