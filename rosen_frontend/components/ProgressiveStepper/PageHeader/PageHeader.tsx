96d70732fe1ec782ef36ad8621fe2cd4463069f3
yuqi88 <snow34545@gmail.com>
2023-04-10 08:41:19 -0700

    add onClick to logo in page header

--- rosen_frontend/components/ProgressiveStepper/PageHeader/PageHeader.tsx
+++ rosen_frontend/components/ProgressiveStepper/PageHeader/PageHeader.tsx
@@ -23,9 +23,16 @@ const styles = {
 };
 
 const PageHeader = () => {
+  const deleteAllData = async () => {
+    const apiUrl = "http://127.0.0.1:8000/videoUpload/admin/wipe_db/"
+    fetch(apiUrl, {method: 'DELETE'})
+        .then(response => response.json())
+        .catch( error => console.error("Error occurred while deleting objects in the database."))
+  }
+
   return (
     <div style={styles.pageContainer}>
-      <a style={styles.innerContainer} href={"/"}>
+      <a style={styles.innerContainer} href={"/"} onClick={deleteAllData}>
         <img style={styles.logo} src="/Rosen_logo.png" alt={"Rosen Logo"} />
       </a>
     </div>
