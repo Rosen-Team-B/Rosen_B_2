477e2aabf1c57c686cb15670a868d446cc2f8972
yuqi88 <snow34545@gmail.com>
2023-04-10 09:09:03 -0700

    modify function

--- rosen_frontend/components/ProgressiveStepper/PageHeader/PageHeader.tsx
+++ rosen_frontend/components/ProgressiveStepper/PageHeader/PageHeader.tsx
@@ -5,8 +5,12 @@ const PageHeader = () => {
   const deleteAllData = async () => {
     const apiUrl = "http://127.0.0.1:8000/videoUpload/admin/wipe_db/"
     fetch(apiUrl, {method: 'DELETE'})
-        .then(response => response.json())
-        .catch( error => console.error("Error occurred while deleting objects in the database.", error))
+        .then(response => {
+            if(response.ok)
+                return true
+            throw new Error('Failed to delete object');
+        })
+        .catch(error => console.error("Error occurred while deleting objects in the database.", error))
   }
 
   return (
