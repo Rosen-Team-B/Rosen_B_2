e1bf9e1e43dbdccc1c221622a57e056ce48081bb
yuqi88 <snow34545@gmail.com>
2023-04-10 20:00:23 -0700

    fix async function

--- rosen_frontend/components/ProgressiveStepper/PageHeader/PageHeader.tsx
+++ rosen_frontend/components/ProgressiveStepper/PageHeader/PageHeader.tsx
@@ -1,18 +1,8 @@
 import React from "react";
 import styles from "./PageHeader.module.css"
+import {deleteAllData} from "../../../utils/deleteAllData"
 
 const PageHeader = () => {
-  const deleteAllData = async () => {
-    const apiUrl = "http://127.0.0.1:8000/videoUpload/admin/wipe_db/"
-    fetch(apiUrl, {method: 'DELETE'})
-        .then(response => {
-            if(response.ok)
-                return true
-            throw new Error('Failed to delete object');
-        })
-        .catch(error => console.error("Error occurred while deleting objects in the database.", error))
-  }
-
   return (
     <div className={styles.pageContainer}>
       <a className={styles.innerContainer} href={"/"} onClick={deleteAllData}>
