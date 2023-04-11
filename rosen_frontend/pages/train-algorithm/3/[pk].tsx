a6f2f8e0b5da1f252696f2b1376d1c3548efbeae
yuqi88 <snow34545@gmail.com>
2023-04-10 22:00:49 -0700

    add loading

--- rosen_frontend/pages/train-algorithm/3/[pk].tsx
+++ rosen_frontend/pages/train-algorithm/3/[pk].tsx
@@ -5,6 +5,9 @@ import {stepperSteps, stepperTexts} from "../../../utils/stepperText";
 import SelectableImage from "../../../components/ProgressiveStepper/selectableImage";
 import PageHeader from "../../../components/ProgressiveStepper/PageHeader/PageHeader";
 import styles from "../../../styles/pages/train-algorithm.module.css";
+import {deleteAllData} from "../../../utils/deleteAllData";
+import CircularProgress from '@mui/material/CircularProgress';
+
 
 interface ImageObject {
     location: string;
@@ -19,25 +22,31 @@ const Step5 = () => {
 
     const [imgObjs, setImgObjs] = React.useState<ImageObject[]>([])
     const [selectedImgObjIds, setSelectedImgObjIds] = React.useState<string[]>([])
+    const [loading, setLoading] = React.useState<boolean>(true)
 
     useEffect(() => {
+        if (!router.query.pk) {
+            return
+        }
         fetch(("http://localhost:8000/videoUpload/reference_image/" + pk + "/deepImageSearch"))
             .then(res => res.json())
             .then(data => {
                 setImgObjs(Object.values(data))
                 setSelectedImgObjIds([])
                 console.log("initialized displayed image path.");
-
             })
             .catch(err => {
                 console.log("error");
             })
+            .finally(() => {
+                setLoading(false)
+            })
     }, [pk])
 
-    const updateSelectedImage = (path:string) => {
+    const updateSelectedImage = (path: string) => {
         const newSelectedImgObjIds = [...selectedImgObjIds]
         const selectImgIndex = newSelectedImgObjIds.indexOf(path)
-        if(selectImgIndex === -1)
+        if (selectImgIndex === -1)
             newSelectedImgObjIds.push(path)
         else
             newSelectedImgObjIds.splice(selectImgIndex, 1)
@@ -47,17 +56,21 @@ const Step5 = () => {
 
     const download = () => {
         const csvHeader = "path,timestamp\n";
-        const csvContent = selectedImgObjIds.reduce( (csvContent, selectImgObjId )=> {
-            const obj = imgObjs.find((imgObj:ImageObject) => imgObj.location === selectImgObjId)
-            console.log(obj,selectImgObjId )
-            return csvContent + `${obj.location},${obj.timestamp}`
-        },  `data:text/csv;charset=utf-8,${csvHeader}`)
+        const csvContent = selectedImgObjIds.reduce((csvContent, selectImgObjId) => {
+            const obj = imgObjs.find((imgObj: ImageObject) => imgObj.location === selectImgObjId)
+            console.log(obj, selectImgObjId)
+            return csvContent + `${obj.location},${obj.timestamp}\n`
+        }, `data:text/csv;charset=utf-8,${csvHeader}`)
         const encodedURI = encodeURI(csvContent);
-        window.open(encodedURI);
+        const windowProxy = window.open();
+        if (windowProxy)
+            windowProxy.location = encodedURI
+
     };
 
-    const finish = () => {
-
+    const finish = async () => {
+        await deleteAllData()
+        router.push("/")
     }
 
     return (
@@ -75,14 +88,31 @@ const Step5 = () => {
             <div className={styles.mainContent}>
                 <h3>{stepperTexts[activeStep]}</h3>
                 <br/>
-                <Grid container rowSpacing={1} columnSpacing={1}>
-                    {imgObjs.map(imgObj => <Grid
-                        item> {SelectableImage(imgObj.location, selectedImgObjIds.includes(imgObj.location), updateSelectedImage)} </Grid>)}
-                </Grid>
-                <div className={styles.nextBtn}>
-                    <Button onClick={finish}>Back to homepage</Button>
-                    <Button onClick={download}> Export CSV</Button>
-                </div>
+                {loading &&
+                    <>
+                        <CircularProgress/>
+                        <div>Loading...</div>
+                    </>
+
+                }
+                {!loading &&
+                    <><Grid container rowSpacing={1} columnSpacing={1} justifyContent={"center"}>
+                        {imgObjs.map(imgObj =>
+                            <Grid key={imgObj.location} item>
+                                <SelectableImage
+                                    pathname={imgObj.location}
+                                    isSelected={selectedImgObjIds.includes(imgObj.location)}
+                                    updateSelectedImage={updateSelectedImage}
+                                />
+                            </Grid>
+                        )}
+                    </Grid>
+                        <div className={styles.nextBtn}>
+                            <Button onClick={finish}>Back to homepage</Button>
+                            <Button onClick={download}> Export CSV</Button>
+                        </div>
+                    </>
+                }
             </div>
         </>
     )
